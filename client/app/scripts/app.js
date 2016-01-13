5749e494956bf33b974f7b4d76430d832714f633
Dean Kerr <dean_k@live.co.uk>
2016-01-13 14:49:21 +0000

    Added socket.io client-side websocket node module to replace angular-websocket

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -1,3 +1,28 @@
 'use strict';
 
-var module = angular.module('bitcoinExchange', ['angular-websocket', 'order-book']);
+var module = angular.module('bitcoinExchange', ['order-book']);
+
+// Sourced from http://www.html5rocks.com/en/tutorials/frameworks/angular-websockets/
+module.factory('socket', function ($rootScope) {
+    var socket = io.connect("http://localhost:1337"); // this should match the port your server opens to websocket traffic
+    return {
+        on: function (eventName, callback) {
+            socket.on(eventName, function () {
+                var args = arguments;
+                $rootScope.$apply(function () {
+                    callback.apply(socket, args);
+                });
+            });
+        },
+        emit: function (eventName, data, callback) {
+            socket.emit(eventName, data, function () {
+                var args = arguments;
+                $rootScope.$apply(function () {
+                    if (callback) {
+                        callback.apply(socket, args);
+                    }
+                });
+            })
+        }
+    };
+});
