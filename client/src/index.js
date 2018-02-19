1b8424a153847c92b297750f8dfca212e34ab6d3
Dean Kerr <dean_k@live.co.uk>
2018-02-19 20:10:34 +0000

    update create-react-app output

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
-import {Provider} from 'react-redux';
-
 import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-import configureStore from './store/configure-store';
-const store = configureStore({ example: { name: 'Joe Bloggs' }});
-
-ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
--- client/src/reducers/root.js
+++
@@ -1,5 +0,0 @@
-import {combineReducers} from 'redux';
-import { reducer as example } from './ducks/example';
-
-const rootReducer = combineReducers({ example });
-export default rootReducer;
